56b4b7e589ad87b780ca4aaab8b9c45c5366e8d5
Jonas Keinholz <git@inyono.dev>
2022-03-10 17:40:34 +0100

    use ESM

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -1,53 +1,70 @@
+/**
+ * This file is part of @inyono/copyright-headers.
+ *
+ * @copyright Copyright (c) 2020-2022 Jonas Keinholz
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/inyono/copyright-headers for the canonical source repository
+ */
 import { generateChangelog } from '@inyono/changelog'
-import fs from 'fs'
-import path from 'path'
-import util from 'util'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const writeFile = util.promisify(fs.writeFile)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 exec().then(() => {
   console.log('done')
 })
 
 async function exec(): Promise<void> {
-  const content = await generateChangelog<undefined>([
-    {
-      tagName: 'v0.0.0',
-      date: '2018-11-08',
-      added: [
-        'Add initial `updateLicenseHeader` function supporting JavaScript, TypeScript, PHP, PHTML and Twig.',
-      ],
+  const content = generateChangelog<undefined>({
+    repository: {
+      firstCommit: 'df12fe2868efc66641034590c3ffd37e0896afbb',
+      owner: 'inyono',
+      repo: 'copyright-headers',
     },
-    {
-      tagName: 'v0.0.1',
-      date: '2018-11-08',
-      deprecated: [
-        '`updateLicenseHeader` will be removed with the next breaking change. Use the new `updateCopyrightHeader` instead.',
-      ],
-      added: [
-        'Add function `updateCopyrightHeader` with slightly different API as a replacement for `updateLicenseHeader`.',
-      ],
-      fixed: [
-        'Handle `language.after` (e.g. `?>` for PHTML) correctly when input already starts with `language.begin` (e.g. `<?php`)',
-      ],
-      internal: ['Add tests.'],
-    },
-    {
-      tagName: 'v0.0.2',
-      date: '2018-11-18',
-      fixed: [
-        'Handle multiple multiline comments without newline in between correctly',
-      ],
-    },
-    {
-      tagName: 'v0.1.0',
-      breakingChanges: [
-        'Remove deprecated `updateLicenseHeader`.',
-        'Drop Node v10 support.',
-      ],
-      added: ['Update dependencies to support Node v14.'],
-    },
-  ])
+    releases: [
+      {
+        tagName: 'v0.0.0',
+        date: '2018-11-08',
+        added: [
+          'Add initial `updateLicenseHeader` function supporting JavaScript, TypeScript, PHP, PHTML and Twig.',
+        ],
+      },
+      {
+        tagName: 'v0.0.1',
+        date: '2018-11-08',
+        deprecated: [
+          '`updateLicenseHeader` will be removed with the next breaking change. Use the new `updateCopyrightHeader` instead.',
+        ],
+        added: [
+          'Add function `updateCopyrightHeader` with slightly different API as a replacement for `updateLicenseHeader`.',
+        ],
+        fixed: [
+          'Handle `language.after` (e.g. `?>` for PHTML) correctly when input already starts with `language.begin` (e.g. `<?php`)',
+        ],
+        internal: ['Add tests.'],
+      },
+      {
+        tagName: 'v0.0.2',
+        date: '2018-11-18',
+        fixed: [
+          'Handle multiple multiline comments without newline in between correctly',
+        ],
+      },
+      {
+        tagName: 'v0.1.0',
+        breakingChanges: [
+          'Remove deprecated `updateLicenseHeader`.',
+          'Drop Node v10 support.',
+        ],
+        added: ['Update dependencies to support Node v14.'],
+      },
+    ],
+  })
 
-  await writeFile(path.join(__dirname, '..', 'CHANGELOG.md'), content)
+  await fs.promises.writeFile(
+    path.join(__dirname, '..', 'CHANGELOG.md'),
+    content
+  )
 }
